9d57e9c4741d52aef8ad4dc9060a276c810f1d0c
Ana Nikolova <ani.nik19@gmail.com>
2019-08-13 17:31:31 +0200

    contact model and get contacts done

--- starter/server.js
+++ starter/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) =>
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/context", require("./routes/context"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
