68931c3c942d7018189951caabe76fa58a53fa4d
Ana Nikolova <ani.nik19@gmail.com>
2019-08-13 15:15:30 +0200

    authenticate Route

--- starter/routes/users.js
+++ starter/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 
