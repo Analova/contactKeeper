310c2498f5d16e5bf27c0ddcfcf0c7c76dca317f
Ana Nikolova <ani.nik19@gmail.com>
2019-08-15 06:38:26 +0200

    add and delete

--- starter/client/src/components/contacts/Contacts.js
+++ starter/client/src/components/contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
 
@@ -6,12 +6,13 @@ const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
   const { contacts } = contactContext;
+
   return (
-    <>
+    <Fragment>
       {contacts.map(contact => (
         <ContactItem key={contact.id} contact={contact} />
       ))}
-    </>
+    </Fragment>
   );
 };
 
