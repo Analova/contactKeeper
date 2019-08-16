1eeb47dbafbe5f1204d0f954990d0b8cc7473f9f
Ana Nikolova <ani.nik19@gmail.com>
2019-08-16 05:29:48 +0200

    edit update Contact Action

--- starter/client/src/context/contact/ContactState.js
+++ starter/client/src/context/contact/ContactState.js
@@ -65,6 +65,9 @@ const ContactState = props => {
   };
 
   // Update contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter contacts
 
@@ -74,11 +77,12 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        currenr: state.current,
+        current: state.current,
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
